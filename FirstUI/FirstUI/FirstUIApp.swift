f59485cb88b3c0ac03de2d49e7aa4493f530489f
Noura Alowayid <nalowayied@gmail.com>
2023-05-15 23:40:50 +0300

    add first swiftUI

---
+++ FirstUI/FirstUI/FirstUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstUIApp.swift
+//  FirstUI
+//
+//  Created by Noura Alowayid on 25/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FirstUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ShowWhatsNew()
+        }
+    }
+}
